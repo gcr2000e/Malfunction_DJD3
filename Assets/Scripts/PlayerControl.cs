5a6d4b34a2a2584362de18060c852bb83370b990
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-03-25 16:29:20 +0000

    Initial player movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,13 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    private CharacterController controller;
+    [SerializeField]
+    private float moveSpeed;
+
+    private Rigidbody rb;
+
+    private bool canMove = false;
+
+    private Vector3 moveDir;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
-        if (controller == null)
-            throw new MissingReferenceException("No Character Controller in Player");
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+            throw new MissingReferenceException("No Rigidbody in Player");
+        else canMove = true;
+
+        // Make sure starting speed is zero
+        moveDir = Vector3.zero;
+    }
+    private void Update()
+    {
+        if (canMove)
+        {
+            // Set input directions to move direction 
+            moveDir.x = InputSystem.actions["Move"].ReadValue<Vector2>().x;
+            moveDir.z = InputSystem.actions["Move"].ReadValue<Vector2>().y;
+
+            rb.linearVelocity = 
+                moveDir.normalized * moveSpeed;
+        }
     }
 }
+
+
