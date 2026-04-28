1eebbe27aa1ff3909bd47c6a4545d7970543d629
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-04-28 14:58:51 +0100

    Initial Damage Test

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField]
+    private uint damage;
+
+    private IHealth health;
+
+    private void Start()
+    {
+        health = GetComponentInParent<IHealth>();
+    }
+
+    // For variable damages
+    public void SetDamage(uint damageValue)
+    {
+        damage = damageValue;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IHealth otherHealth = other.GetComponent<IHealth>();
+
+        // Prevent attacking own health system
+        if (otherHealth != health)
+        {
+            // Damage other
+            otherHealth.Damage(damage);
+        }
+    }
+}
