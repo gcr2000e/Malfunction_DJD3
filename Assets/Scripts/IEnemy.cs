840f9b6057edd95eada6109988bd50888570ee41
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-04-30 08:48:56 +0100

    Created IEnemy

---
+++ Assets/Scripts/IEnemy.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public abstract class IEnemy : MonoBehaviour
+{
+    [Header("Movement")]
+    private Transform target;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private bool canMove = true;
+
+    protected Rigidbody rb;
+
+    [Header("Combat")]
+    [SerializeField]
+    private uint attackStrenght;
+    [SerializeField]
+    private float attackDistance;
+
+    [Header("Visual")]
+    [SerializeField]
+    private GameObject model;
+
+    protected Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Get player as target when spawned
+        target = FindFirstObjectByType<PlayerControl>()
+            .gameObject
+            .transform;
+    }
+
+    private void ModelRotation()
+    {
+        Vector3 lookAtPos = target.position;
+        lookAtPos.y = model.transform.position.y;
+
+        // Apply rotation to model
+        model.transform.LookAt(lookAtPos);
+    }
+
+    private bool InRange()
+    {
+        // Get the distance between the two
+        float distance =
+            (target.position - transform.position)
+            .magnitude;
+        // If the distance is less or equal
+        return (distance <= attackDistance);
+    }
+
+    protected abstract void Attack();
+    protected abstract void DoMovement();
+}
