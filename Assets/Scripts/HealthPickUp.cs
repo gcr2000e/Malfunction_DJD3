29ab9b72f79bd477f8a1325df1e549845bf0b54a
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-05-04 17:49:08 +0100

    Health pickup and health rebalance

---
+++ Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour
+{
+    [SerializeField]
+    private uint value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = 
+            other.GetComponent<PlayerHealth>();
+
+        if (playerHealth != null)
+        {
+            playerHealth.Heal(value);
+            Destroy(gameObject);
+        }
+    }
+}
