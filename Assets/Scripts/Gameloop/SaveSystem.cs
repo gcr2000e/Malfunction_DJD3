ae15086400543c5d62f9f917e884e0124bb04699
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-06-04 09:26:40 +0100

    Save system manages generation now

--- Assets/Scripts/Gameloop/SaveSystem.cs
+++ Assets/Scripts/Gameloop/SaveSystem.cs
@@ -7,11 +7,17 @@ public class SaveSystem : MonoBehaviour
 
     private PlayerCombat pCombat;
     private PlayerHealth pHealth;
+    private GeneratorManager pcg;
 
     private void Start()
     {
         pCombat = FindAnyObjectByType<PlayerCombat>();
         pHealth = FindAnyObjectByType<PlayerHealth>();
+        pcg = FindAnyObjectByType<GeneratorManager>();
+
+        // If no save game then generate random
+        pcg.Generate();
+        // If there is a save game generate with the seed
     }
 
     public void SetLevel(int level)
