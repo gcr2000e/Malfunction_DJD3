abb50c4dae881738f53ffcb42af9002ac76b24e1
Inês Gomes <inesgomes.pessoal05@gmail.com>
2026-06-13 14:59:38 +0100

    Game loading and saving updated

---
+++ Assets/Scripts/Gameloop/LoadGame.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class LoadGame : MonoBehaviour
+{
+    private string savePath =>
+        Path.Combine(Application.persistentDataPath, "save.json");
+    private string persistentDataPath =>
+    Path.Combine(Application.persistentDataPath, "persistentData.json");
+
+    public void LoadLevel()
+    {
+        if (File.Exists(savePath))
+        {
+            SaveData data =
+                JsonUtility
+                .FromJson<SaveData>(
+                    File.ReadAllText(savePath));
+
+            SceneManager.LoadScene(data.currentLevel);
+        }
+    }
+
+    public void NewGame()
+    {
+        if (File.Exists(savePath))
+            File.Delete(savePath);
+        if (File.Exists(persistentDataPath))
+            File.Delete(persistentDataPath);
+        SceneManager.LoadScene("Level01");
+    }
+}
